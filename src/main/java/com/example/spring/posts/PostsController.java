ab327ba9e461d7965d6bde4ba6e9a1c4f6f542f8
정필성 <ibetter.kr@gmail.com>
2025-03-01 16:52:13 +0900

    코드 수정

--- src/main/java/com/example/spring/posts/PostsController.java
+++ src/main/java/com/example/spring/posts/PostsController.java
@@ -1,13 +1,14 @@
 package com.example.spring.posts;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class PostsController {
 
     // 게시글 등록 (화면, GET)
-    @GetMapping("/posts/create")
+    @RequestMapping(value = "/posts/create", method = RequestMethod.GET)
     public String create() {
         return "posts/create";
     }
